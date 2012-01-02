ad510c02ecde7ac025196351ee96e3948ac7508f
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2012-01-02 22:52:27 +0800

    updated

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,7 @@ cp build/libs/*.jar lib-signed
 read -p "Storepass: " storepass
 
 find lib-signed -name "*.jar" -exec jarsigner -storepass $storepass {} copad \; -print
+
+s3cmd sync lib-signed/* s3://s3.copad.cc/lib/
+
+rm -rf lib-signed
